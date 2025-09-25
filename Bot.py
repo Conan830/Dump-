8452b0b672469c6c387d0ecad244ce281f46fdaa
Detective Conan <109863157+Conan830@users.noreply.github.com>
2025-09-25 19:36:34 +0530

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,13 +1,30 @@
-BOT_TOKEN = str(getenv('BOT_TOKEN' , '7584718311:AAHgM2r456Sz6MDCbx9uRsgRXcNPnL73se4'))
-    PICS = (environ.get('PICS', 'https://i.ibb.co/M5JZr5CD/2374a6296671600a8846f2ba3ea9a035.jpg')).split()
-    name = str(getenv('name', 'iptv'))
-    OWNER_ID = [int(x) for x in os.environ.get("OWNER_ID", "7815873054").split()]
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'Kadali_mahesh'))
+class TG_CONFIG:
+    api_id = 9976721
+    dburl = "mongodb+srv://hello:hello@cluster0.vc2htx0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    api_hash = "8409278629:AAE7cSIN3mfT1SakLY_Lrge8aXxdkrcU"
+    stringhi = "JT_luclVOxsK_bERZj3VLuBO0NyjJ0E7pPk5X6s7PehAvfiSP9FSkhl34ghLpaP0imNvyOteNJH7XACqE0VyMzdoL9lfg0pdLFRjQD7G694h0Y2KeMAgYBhtoGwS30I0U_MtDuxc0q5U3Y2ApeSiqcPdyloi_dZIJzwAAAABHP54rAA"
+    UPSTREAM_REPO = "https://github.com/MAHESH-KADALI/Record-bot"
+    UPSTREAM_BRANCH = "main"
+    bot_token = "8043589347:AAFnzvdIY-TBET_DN-zTsiUqLz-9eSU0Rb4"
+    owner_id = 7815873054
+    #DEVS or #OWNERS
+    sudo_users = [7172796863]
+    session = "M3u8_Recording_Bot"
+    session2 = "NiteshTeamUniverse"
+    token_timeout = 3600
+    TESTOVER = True
+    bot_creater = "Not Known"  # Don't Remove if you Respect the DEV
 
-    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://hello:hello@cluster0.vc2htx0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'Anime Time')) 
-     
-    BAN_ALERT = str(getenv('BAN_ALERT' , '<b>ʏᴏᴜʀ ᴀʀᴇ ʙᴀɴɴᴇᴅ ᴛᴏ ᴜsᴇ ᴛʜɪs ʙᴏᴛ.ᴄᴏɴᴛᴀᴄᴛ @Kadali_mahesh ᴛᴏ ʀᴇsᴏʟᴠᴇ ᴛʜᴇ ɪssᴜᴇ!!</b>'))
-    SHORTLINK = is_enabled('SHORTLINK', True)
-    SHORTLINK_URL = getenv('SHORTLINK_URL', 'atglinks.com')
-    SHORTLINK_API = getenv('SHORTLINK_API', '2eaf56483066a41fdf906aa08fe0fba0ee0486de')
+    bot_creater_id = "@team"  # Don't Remove if you Respect the DEV
+
+    START_MSG = """
+<b>Hello <code>@{}</code>,
+
+I'm a simple m3u8 recorder bot</b>
+
+> <code>{}</code>
+
+<b>Use Me Here @maheshsirop</b>
+
+<b>Made by https://t.me/+BjPNuHsuDf9hMzk1</b>
+    """
